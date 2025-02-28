554a45113de3b82c2d8b21466c0353b8c327aecb
Mohsen Khademhosseini <mohsenpm77@gmail.com>
2025-02-28 20:04:30 +0330

    feat: create sideBar comps

--- src/constants/assetsPath.ts
+++ src/constants/assetsPath.ts
@@ -3,5 +3,13 @@ export const ASSETS = {
   home: '/i/home.svg',
   helpOutline: '/i/help-outline.svg',
   accountCircle: '/i/account-circle.svg',
-  arrowRight: '/i/arrow-right.svg'
+  arrowRight: '/i/arrow-right.svg',
+  ham: '/i/ham.svg',
+  exit: '/i/exit.svg',
+  dashboard: '/i/dashboard.svg',
+  adminPanel: '/i/admin-panel.svg',
+  school: '/i/school.svg',
+  support: '/i/support.svg',
+  notification: '/i/notification.svg',
+  payment: '/i/payment.svg'
 };
